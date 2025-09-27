c8f2609f204555530b3f1c79a5cc29c0357232bf
Joga Ram <laptoplenovoid04112024@gmail.com>
2025-09-27 14:02:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,17 +22,18 @@ API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_DB = os.getenv("MONGO_DB", "")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7054481969").split())) # list seperated via space
-DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
+DB_NAME = os.getenv("DB_NAME", "telegram_downloaders")
 STRING = os.getenv("STRING", None) # optional
 LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002974996858")) # optional with -100
 FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002974996858")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "") # for decryption
+MASTER_KEY = os.getenv("MASTER_KEY", "Vhigdysthjbgjjhfccfhhnhfxxyyxfbbbjjzggtzbb") # for session encryption
+IV_KEY = os.getenv("IV_KEY", "s7Yx5CkVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "10"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/+8b8O7HoOUco0NGE1") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/ImInvisible00")
+
 
 
